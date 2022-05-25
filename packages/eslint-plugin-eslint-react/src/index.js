9b21229f35f5883848c3cb6cb01d2092eb4e73cc
gunnarx2 <tobbelindstrom93@gmail.com>
2022-05-25 07:26:14 +0200

    Testing

--- packages/eslint-plugin-eslint-react/src/index.js
+++ packages/eslint-plugin-eslint-react/src/index.js
@@ -1,17 +1,7 @@
 /* eslint-disable global-require */
-const allRules = {
-  'interactive-solutions/id-filename-mismatch': require('./rules/id-filename-mismatch'),
-  'interactive-solutions/id-property-mismatch': require('./rules/id-property-mismatch')
-};
-
 module.exports = {
-  rules: allRules,
-  configs: {
-    all: {
-      rules: {
-        'interactive-solutions/id-filename-mismatch': 'error',
-        'interactive-solutions/id-property-mismatch': 'error'
-      }
-    }
+  rules: {
+    'id-filename-mismatch': require('./rules/id-filename-mismatch'),
+    'id-property-mismatch': require('./rules/id-property-mismatch')
   }
 };
